a52a994386bd380ac4e536a1fd49f45824d390e7
Benjamin Hines <75751722+Lubba-64@users.noreply.github.com>
2021-03-25 22:32:09 -0400

    Update bot.js
    added som commands :")

--- bot.js
+++ bot.js
@@ -27,6 +27,19 @@ function CheckCommands(msg){
     words = words.slice(1)
     if (words.length > 0){
         var command = words[0];
+        if (msg.author.username == superadmin){
+            switch(words[0].toLowerCase()){
+                case "switchroom":
+                    msg.reply("switching...")
+                    msg.delete()
+                    let uuid = words[1].replace("https://","");
+                    uuid = uuid.replace("dogehouse.tv/room/","");
+                    console.log(uuid);
+                    app.rooms.join(uuid);
+                    break;
+            }
+        }
+
         if (command.toLowerCase() == "admin" && (admin.filter(word => word == msg.author.username).length > 0 || msg.author.username == superadmin)){//)){
             var adminCommand = words[1];
             switch(adminCommand.toLowerCase()){
@@ -35,23 +48,27 @@ function CheckCommands(msg){
                     if (words[2] != undefined){
                         prefix = words[2];
                         msg.reply("Changed Prefix to " + prefix);
+                        msg.delete();
                     }
                     else{
-                        msg.reply("cannot change prefix to undefined")
+                        msg.reply("cannot change prefix to undefined");
+                        msg.delete();
                     }
                 break;
                 case "add":
                     var username = words[2].replace(/@/g,"");
                     admin.push(username);
                     msg.reply("added " + username + " as an admin");
+                    msg.delete();
                     console.log(username);
                     break;
                 case "remove":
                     var username = words[2].replace(/@/g,"");
                     admin.splice(admin.indexOf(username));
                     msg.reply("removed " + username + " as an admin");
-
+                    msg.delete();
                     break;
+
             }
         }
         else{
@@ -59,14 +76,17 @@ function CheckCommands(msg){
                 case "help":
                     msg.reply(`\n
                     do <command> help for more info on that command\n
-                    CURRENT_COMMANDS:\n
-                    remind
+                    𝖈𝖚𝖗𝖗𝖊𝖓𝖙_𝖈𝖔𝖒𝖒𝖆𝖓𝖉𝖘:\n
+                    remind, \n
+                    roll, \n
+                    spam 
                     `
-                    ,{mentionUser: false, whispered: true})
+                    ,{mentionUser: false, whispered: true});
+                    msg.delete();
                     break;
                 case "remind":
                 case "reminder":
-                    let subCommand = words[1].toLowerCase();
+                    var subCommand = words[1].toLowerCase();
                     if (subCommand == "help"){
                         msg.reply(
                             `\n
@@ -75,24 +95,64 @@ function CheckCommands(msg){
                             USE: Reminds the user of something after a certain amount of time\n
                             `
                         ,{mentionUser: false, whispered: true})
+                        msg.delete();
                         return;
                     }
                     else if (subCommand.length == 5){
-                        var converted = ConvertTime(words[1])
+                        var converted = ConvertTime(words[1]);
                         if (converted == NaN){
                             msg.reply(`
                             please make sure the time is in this format: HH:MM\n
-                            for example:
-                            99:99 is 99 hours, and 99 minutes
+                            EX: remind 99:99 <message> will remind you of <message> 99 hours, and 99 minutes
 
-                            `,{mentionUser: false, whispered: true})
+                            `,{mentionUser: false, whispered: true});
+                            msg.delete();
                             return;
                         }
                         else{
                             msg.reply("reminder recorded!",{mentionUser: false, whispered: true});
                             words = words.splice(2,words.length);
                             sendReminder("@" + msg.author.username + " " + words.join().replace(/,/g," "), converted);
+                            msg.delete();
                             return;
+                        }
+                    }
+                case "roll":
+                    var subCommand = words[1].toLowerCase();
+                    if (subCommand == "help"){
+                        msg.reply(`
+                            roll <integer 2-100000> to get a random number between one and the specified number
+                            EX: roll 100 is a 1/100 chance to get the number 1
+                            `,{mentionUser: false, whispered: true});
+                            msg.delete();
+                        return;
+                    }
+                    else{
+                        let rollamount = parseInt(subCommand);
+                        if (rollamount != NaN){
+                            msg.reply(Math.round(Math.random()*subCommand));
+                            msg.delete();
+                        }
+                        else{
+                            msg.reply("thats not an integer",{mentionUser: false, whispered: true});
+                            msg.delete();
+                        }
+                    }
+                    break;
+                case "spam":
+                    var subCommand = words[1].toLowerCase();
+                    if (subCommand == "help"){
+                        msg.reply(`
+                        spams the sentence attached 5 times over 10 seconds\n
+                        EX: spam  :Pepega:  :Pepega:  :Pepega:  :Pepega:  :Pepega:  :Pepega:  :Pepega:
+                        `)
+                        msg.delete();
+                    }
+                    else{
+                        words = words.splice(2,words.length);
+                        let message = words.join().replace(/,/g," ")
+                        for (let i = 0; i < 6; i++){
+                            setTimeout(function(){app.bot.sendMessage(message)},2000*i);
                         }
                     }
                     break;
